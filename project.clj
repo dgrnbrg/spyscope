5f6c9bd38939ab7da1e66a076a458bc3c8aaa1e3
Swaroop C H <swaroop@swaroopch.com>
2014-06-03 14:50:30 +0530

    Updating dependencies version numbers

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/dgrnbrg/spyscope"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-time "0.5.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-time "0.7.0"]])
