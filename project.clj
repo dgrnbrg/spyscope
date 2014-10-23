c90e1db4d2b5013319d8fa3929ae22ca4dc8931f
David Greenberg <dsg123456789@gmail.com>
2014-10-23 14:27:12 -0400

    Make it easier to release

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject spyscope "0.1.4"
+(defproject spyscope "0.1.5-SNAPSHOT"
   :description "Trace-oriented debugging tools for Clojure"
   :url "http://github.com/dgrnbrg/spyscope"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories  [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.7.0"]])
